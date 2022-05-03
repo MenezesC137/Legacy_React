7fcb134e7ef91e9ff7b040eb50608c5920c0c486
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-05-03 10:04:19 -0300

    Comp.Filho

---
+++ Exercises_React/ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='João' lastName='silva'/>
+        <Member name='Clara' lastName='silva'/>
+        <Member name='Maria' lastName='silva'/>
+        <Member name='José' lastName='silva'/>
+    </div>
+)
