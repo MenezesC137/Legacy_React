3c2c37b14785d1e68b74f338e34c28758078c1d2
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-05-04 10:37:29 -0300

    Integrando react-redux #03

---
+++ Exercises_React/ex/fieldReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = { value: 'opa'}
+
+export default function(state = INITIAL_STATE, action) {
+
+    switch(action.type){
+        case 'VALUE_CHANGED':
+            return {
+                ...state,
+                value: action.payload,
+            }
+        default:
+            return state
+    }
+
+}
